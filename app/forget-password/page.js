4c5b237e5f115256e2fe55a4b6416d5d1701b6ca
haniful360 <haniful360@gmail.com>
2024-12-04 17:34:12 +0600

    add forget password page

--- app/forget-password/page.js
+++ app/forget-password/page.js
@@ -7,9 +7,9 @@ import eyeoff from "@/public/icon/eye-off.svg"; // Add your eye-off icon here
 import Image from "next/image";
 import Link from "next/link";
 
-const SignIn = () => {
+const ForgetPassword = () => {
 
-  const [showPassword, setShowPassword] = useState(false);
+  const [forgetPassword, setForgetPassword] = useState(false);
 
   return (
     <div>
@@ -23,43 +23,33 @@ const SignIn = () => {
       <div className="flex items-center justify-center bg-gray-100 py-12">
         <div className="w-full max-w-[600px] bg-white rounded-lg shadow-lg p-8">
           <h2 className="text-2xl font-bold text-center mb-6">
-            Sign In
+            Forget Password
           </h2>
 
           <form>
-            {/* Email Input */}
-            <div className="mb-4">
-              <input
-                type="email"
-                id="email"
-                className="w-full border border-gray-300 rounded-[8.73px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
-                placeholder="Email"
-                required
-              />
-            </div>
-
+           
             {/* Password Input */}
             <div className="mb-4 relative">
               <input
-                type={showPassword ? "text" : "password"}
+                type={forgetPassword ? "text" : "password"}
                 id="password"
                 className="w-full border border-gray-300 rounded-[8.73px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
                 placeholder="Password"
                 required
               />
-              {showPassword ? (
+              {forgetPassword ? (
                 <Image
                   src={eyeoff}
                   alt="Hide password"
                   className="absolute top-2.5 right-4 h-5 w-5 cursor-pointer"
-                  onClick={() => setShowPassword((prev) => !prev)}
+                  onClick={() => setForgetPassword((prev) => !prev)}
                 />
               ) : (
                 <Image
                   src={eyeIcon}
                   alt="Show password"
                   className="absolute top-2.5 right-4 h-5 w-5 cursor-pointer"
-                  onClick={() => setShowPassword((prev) => !prev)}
+                  onClick={() => setForgetPassword((prev) => !prev)}
                 />
               )}
             </div>
@@ -68,39 +58,13 @@ const SignIn = () => {
 
             {/* Submit Button */}
             <button className="w-full  py-2 bg-[#EA5326] text-white font-bold rounded-[62px] hover:bg-orange-600 transition-colors">
-              Sign In
+             Submit
             </button>
-
-            {/* Google Button */}
-            <div className="text-center mt-4">
-              <button className="flex items-center font-semibold justify-around w-full  py-2 border border-gray-300 rounded-[8.73px]">
-                <Image
-                  src={google}
-                  alt="Google"
-                  className="h-5 w-5 mr-2"
-                  width={24}
-                  height={24}
-                />
-                Continue with Google
-              </button>
-            </div>
           </form>
-
-          {/* Sign In Link */}
-          <div className="text-center mt-6">
-            <p className="text-sm md:text-xl">
-              Already have an account?
-              <Link
-                href="/sign-up"
-                className="text-orange-500 hover:underline font-semibold"
-              > Sign up
-              </Link>
-            </p>
-          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default SignIn;
+export default ForgetPassword;
