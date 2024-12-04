05a003ca11b07f3d0873bb0f4488ca3403a96a60
haniful360 <haniful360@gmail.com>
2024-12-04 10:35:45 +0600

    banner image size update

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React from "react";
 import banner from "@/public/images/banner.svg";
 import gadgets from "@/public/images/gadget.png";
+import RightArrow from "../svg/RightArrow";
+import Link from "next/link";
 
 // Load Figtree font
 const figtree = Figtree({
@@ -18,8 +20,9 @@ const Banner = () => {
         <h2 className="text-[40px] sm:text-[50px] md:text-[60px] lg:text-[80px] text-[#232323] font-bold leading-[1.2] lg:leading-[99px] mb-4 text-center">
           The Best High-Quality
           <span className="text-[#232323] font-medium">
-            <br /> Home Appliance </span>
-           Products
+            <br /> Home Appliance
+          </span>
+          Products
         </h2>
         <p className="text-[#232323] text-[20px] mb-8">
           100+ Collections for your outfit inspirations this summer
@@ -28,9 +31,10 @@ const Banner = () => {
           View Category
         </button>
 
-        <div className="flex flex-col md:flex-row justify-center items-center p-4 bg-gray-50 rounded-lg">
-          {/* Left Image */}
-          <div className="relative w-full md:w-[1168px]">
+        {/* banner image */}
+        <div className="flex flex-col md:flex-row justify-center items-center p-4 bg-gray-50 rounded-lg mt-8">
+          {/* Left Image */} 
+          <div className="relative w-full md:w-[1168px] h-[442px]">
             <Image
               src={banner}
               alt="Kitchen Appliance"
@@ -61,7 +65,7 @@ const Banner = () => {
           </div>
 
           {/* Right Image */}
-          <div className="w-full md:w-[332px] mt-4 md:mt-0 md:ml-4">
+          <div className="w-full md:w-[332px] h-[442px] mt-4 md:mt-0 md:ml-4">
             <Image
               src={gadgets}
               alt="Electronics Gadgets"
@@ -69,28 +73,19 @@ const Banner = () => {
               height={500}
               className="w-full h-auto rounded-lg"
             />
-            <div className="bottom-4 left-4">
-              <h3 className="text-lg font-semibold mb-2">
+            <div>
+              <h3 className="text-lg text-[#232323] font-semibold my-3">
                 Electronics Gadgets
               </h3>
             </div>
-            <button className="border border-red-500 text-red-500 bg-white py-2 px-4 rounded-full flex items-center shadow-md">
-              <span className="mr-2">Shop Now</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-4 h-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.5 4.5l6 6m0 0l-6 6m6-6H3"
-                />
-              </svg>
-            </button>
+            <Link href="">
+              <button className="border border-red-500 text-red-500 bg-white py-2 px-4 sm:px-6 md:px-8 lg:px-10 rounded-full flex justify-center items-center w-full sm:w-[220px] md:w-[280px] lg:w-[332px] transition-all duration-300 hover:bg-red-500 hover:text-white">
+                <span className="mr-2 text-sm sm:text-base md:text-lg">
+                  Shop Now
+                </span>
+                <RightArrow />
+              </button>
+            </Link>
           </div>
         </div>
       </div>
---
+++ components/svg/RightArrow.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const RightArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="m10 17l5-5l-5-5"
+      ></path>
+    </svg>
+  );
+};
+
+export default RightArrow;
