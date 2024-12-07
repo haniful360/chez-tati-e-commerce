7f62f1a46e78cfb77ae04402288a9b1435b534d3
haniful360 <haniful360@gmail.com>
2024-12-07 19:23:20 +0600

    add user profile

---
+++ components/svg/NewCartIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NewCartIcon = () => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.24195 11.7276H5.78741L3.48438 24.3943H24.2116L21.9086 11.7276H18.4541M9.24195 11.7276V8.27305C9.24195 5.7292 11.3042 3.66699 13.848 3.66699V3.66699C16.3919 3.66699 18.4541 5.7292 18.4541 8.27305V11.7276M9.24195 11.7276H18.4541M9.24195 11.7276V15.1821M18.4541 11.7276V15.1821"
+        stroke="#CCCCCC"
+        strokeWidth="1.72727"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default NewCartIcon;
---
+++ components/svg/UserHeartIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const UserHeartIcon = () => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.8485 24.6078C-9.18138 11.8788 6.93982 -1.93942 13.8485 6.79834C20.758 -1.93943 36.8792 11.8788 13.8485 24.6078Z"
+        stroke="#CCCCCC"
+        strokeWidth="1.72727"
+      />
+    </svg>
+  );
+};
+
+export default UserHeartIcon;
